97cd04dbe5059be3ba9bc04770c864742322b2d4
Pontus Thomé <pontusthome@gmail.com>
2016-05-25 14:59:33 +0200

    A simple loading screen

---
+++ src/app/components/watsonEntityAnalyzerApi/search.resource.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('watsonEntityAnalyzer')
+        .factory('Analyzer', Analyzer);
+
+    function Analyzer(Resource) {
+        //return new Resource('/search/:search', { search: '@search' });
+    }
+
+})();
